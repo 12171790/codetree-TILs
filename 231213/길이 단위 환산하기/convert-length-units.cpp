78056adf0f06413b317c5223613e6c9217e829ce
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-13 11:37:24 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231213/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double fitRatio = 30.48;
+    double ft;
+    
+    cin >> ft;
+    cout << fixed;
+    cout.precision(1);
+    cout << ft * fitRatio;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
