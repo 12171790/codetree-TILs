1fc5d2856393c040b2825cbbda8058b9e0e9a435
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-13 17:33:11 +0900

    Title: 두 개의 직각삼각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231213/두 개의 직각삼각형/two-right-triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+    cnt = n;
+    //별 ... 8 6 4 2
+    //공백 0 2 4 6 ...
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 2 * (n - i); j++) {
+            if (j == cnt) {
+                for (int k = 0; k < 2 * i; k++) {
+                    cout << " ";
+                }
+            }
+            cout << "*";
+        }
+        cnt--;
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
