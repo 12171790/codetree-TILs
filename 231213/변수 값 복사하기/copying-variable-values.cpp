33f6fad17a68bc289188fabb22fef46bc6c0680f
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-13 11:19:59 +0900

    Title: 변수 값 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231213/변수 값 복사하기/copying-variable-values.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    a = b = c = 3;
+
+    std::cout << a << " " << b << " " << c << std::endl;
+    
+    return 0;
+}
