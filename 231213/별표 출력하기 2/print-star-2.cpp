c89bdca7092edd28bd0836ae5b788c91b45cde1a
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-13 13:56:06 +0900

    Title: 별표 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231213/별표 출력하기 2/print-star-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = n; i > 0; i--) {
+        for (int j = i; j > 0; j--) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
