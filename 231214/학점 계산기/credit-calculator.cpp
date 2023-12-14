f5610e8d9a800cd8db290a4633ed997fc7443cc5
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-14 16:37:52 +0900

    Title: 학점 계산기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231214/학점 계산기/credit-calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double arr[5] = {};
+    int n;
+    double avg = 0, sum = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    avg = sum / n;
+    cout << fixed;
+    cout.precision(1);
+    cout << avg << endl;;
+    if (avg > 4) cout << "Perfect";
+    else if (avg > 3) cout << "Good";
+    else cout << "Poor";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
