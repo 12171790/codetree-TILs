c245831b1da828e9a95ec6449e61ac79950ed9a7
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-14 18:01:47 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231214/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2, temp;
+    cin >> n1 >> n2;
+
+    cout << n1 << " " << n2 << " ";
+    for (int i = 0; i < 8; i++) {
+        temp = n1 + n2;
+        n1 = n2;
+        n2 = temp;
+
+        cout << temp % 10 << " ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
