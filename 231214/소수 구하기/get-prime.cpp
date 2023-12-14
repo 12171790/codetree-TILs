38438fec2d6ef18c1809150370b738d816fcf44f
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-14 15:30:01 +0900

    Title: 소수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231214/소수 구하기/get-prime.cpp
+++ 231214/소수 구하기/get-prime.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main() {
-    int n, a = 0;
+    int n;
     cin >> n;
+    bool arr[n + 1];
 
-    for (int i = 2; i <= n; i++) {
-        for (int j = 2; j <= i - 1; j++) {
-            if (i % j == 0) {
-                a = 1;
-                break;
-            }
+    arr[0] = arr[1] = true;
+    //초기화
+    for (int i = 2; i < n + 1; i++) arr[i] = true;
+
+    for (int i = 2; i < sqrt(n); i++) {
+        if (arr[i]) {
+            for (int j = i + i; j < n; j += i) arr[j] = false;
         }
-        if (a == 0) cout << i << " ";
-        else a = 0;
     }
+
+    for (int i = 2; i < n; i++) {
+        if (arr[i]) cout << i << " ";
+    }
+
     // 여기에 코드를 작성해주세요.
     return 0;
 }
