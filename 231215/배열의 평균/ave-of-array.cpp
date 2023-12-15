b324dc0dcf2a79c090348cbae69e4c13b301000a
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-15 12:07:50 +0900

    Title: 배열의 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231215/배열의 평균/ave-of-array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[2][4] = {};
+    int totalSum = 0, heightSum = 0, widthSum = 0;
+    double totalAvg, heightAvg, widthAvg;
+    cout << fixed;
+    cout.precision(1);
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> arr[i][j];
+            widthSum += arr[i][j];
+        }
+        totalSum += widthSum;
+        widthAvg = widthSum / 4;
+        widthSum = 0;
+        cout << widthAvg << " ";
+    }
+    cout << endl;
+
+    for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < 2; i++) {
+            heightSum += arr[i][j];  
+        }
+        heightAvg = heightSum / 2;
+        heightSum = 0;
+        cout << heightAvg << " ";
+    }
+    cout << endl;
+    totalAvg = totalSum / 8;
+    cout << totalAvg << endl;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
