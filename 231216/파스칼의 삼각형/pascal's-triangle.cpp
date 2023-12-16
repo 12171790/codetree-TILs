4af137ecc3fff05a962f4b5a7ec7d19f52e98970
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-16 15:38:42 +0900

    Title: 파스칼의 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231216/파스칼의 삼각형/pascal's-triangle.cpp
+++ 231216/파스칼의 삼각형/pascal's-triangle.cpp
@@ -12,9 +12,7 @@ int main() {
         arr[i] = (int*)malloc(sizeof(int*) * n);
     }
 
-    arr[0][0] = arr[1][0] = arr[1][1] = 1;
-
-    for (int i = 2; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j <= i; j++) {
             if (j == 0 || j == i) arr[i][j] = 1;
             else arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
