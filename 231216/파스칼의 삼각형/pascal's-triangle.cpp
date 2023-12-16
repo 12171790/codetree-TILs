8505efdfedd36feff7ceed44eea4a0b86daf8dc4
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-16 15:34:52 +0900

    Title: 파스칼의 삼각형, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231216/파스칼의 삼각형/pascal's-triangle.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+    int n;
+    int** arr;
+
+    cin >> n;
+    arr = (int**)malloc(sizeof(int*) * n);
+    for(int i = 0; i < n; i++) {
+        arr[i] = (int*)malloc(sizeof(int*) * n);
+    }
+
+    arr[0][0] = arr[1][0] = arr[1][1] = 1;
+
+    for (int i = 2; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (j == 0 || j == i) arr[i][j] = 1;
+            else arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
