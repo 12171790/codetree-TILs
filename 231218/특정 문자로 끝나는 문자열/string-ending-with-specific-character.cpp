72879716b318914278a93c9fea39819f3b6da3ad
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-18 10:15:11 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231218/특정 문자로 끝나는 문자열/string-ending-with-specific-character.cpp
+++ 231218/특정 문자로 끝나는 문자열/string-ending-with-specific-character.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     char alp = 0;
     char lastAlp = 0;
+    int cnt = 0;
     string strArr[10] = {};
 
     for (int i = 0; i < 10; i++) {
@@ -13,8 +14,12 @@ int main() {
 
     for (int i = 0; i < 10; i++) {
         lastAlp = strArr[i][strArr[i].length() - 1];
-        if (alp == lastAlp) cout << strArr[i] << endl;
+        if (alp == lastAlp) {
+            cout << strArr[i] << endl;
+            cnt++;
+        }
     }
+    if (cnt == 0) cout << "None" << endl;
     // 여기에 코드를 작성해주세요.
     return 0;
 }
