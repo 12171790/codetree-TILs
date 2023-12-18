2b3a76034886dc18afa0746f8b8f583815b80ca2
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-18 16:14:56 +0900

    Title: 아스키코드의 합과 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231218/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    char a, b;
+    cin >> a >> b;
+    cout << abs(a + b) << " " << abs(a - b) << endl;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
