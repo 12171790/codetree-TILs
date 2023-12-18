94fcbed94867754855a7e5303965c9ba882d4867
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-18 10:02:23 +0900

    Title: 문자열 길이의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231218/문자열 길이의 합/sum-length-of-string.cpp
+++ 231218/문자열 길이의 합/sum-length-of-string.cpp
@@ -11,7 +11,10 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> strArr[i];
         for (int j = 0; j < strArr[i].length(); j++) {
-            if (strArr[i][j] == 'a') cnt++;
+            if (strArr[i][j] == 'a') {
+                cnt++;
+                break;
+            }
         }
         sum += strArr[i].length();
     }
