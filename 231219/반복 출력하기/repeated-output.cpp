90b2ce5b1321030b313497539c432d84eafa93be
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-19 09:04:26 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231219/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void Printer(int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    int eNum = 0;
+    cin >> eNum;
+    Printer(eNum);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
