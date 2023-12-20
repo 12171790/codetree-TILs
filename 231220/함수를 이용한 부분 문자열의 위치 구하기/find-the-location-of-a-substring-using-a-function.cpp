8c654196d52f9fe10dc2a36b97f3ffcafeea6e01
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-20 09:16:00 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231220/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
+++ 231220/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 int SearchPartString(string str, string tarStr) {
-    int cnt;
-    for (int i = 0; i <= str.length() - tarStr.length(); i++) {
-        for (int j = 0; j < tarStr.length(); j++) {
-            if (str[i + j] == tarStr[j]) cnt++;
-            else break;
-        }
-        if (cnt == tarStr.length()) return i;
-        else cnt = 0;
-    }
-    return -1;
+    // int cnt;
+    // for (int i = 0; i <= str.length() - tarStr.length(); i++) {
+    //     for (int j = 0; j < tarStr.length(); j++) {
+    //         if (str[i + j] == tarStr[j]) cnt++;
+    //         else break;
+    //     }
+    //     if (cnt == tarStr.length()) return i;
+    //     else cnt = 0;
+    // }
+    // return -1;
+    if(str.find(tarStr) != string::npos) return str.find(tarStr);
+    else return -1;
 }
 
 int main() {
