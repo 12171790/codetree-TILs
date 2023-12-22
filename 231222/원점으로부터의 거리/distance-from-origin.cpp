406d35d09896741b41dcf844f97a15067935c86f
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-22 10:06:19 +0900

    Title: 원점으로부터의 거리, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231222/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Dot {
+private:
+	int x;
+	int y;
+	int num;
+public:
+	Dot(int x = 0, int y = 0, int num = 0) {
+		SetX(x);
+		SetY(y);
+		this->num = num;
+	}
+
+	void SetX(int x) { this->x = x; }
+	void SetY(int y) { this->y = y; }
+
+	int GetX() { return this->x; }
+	int GetY() { return this->y; }
+
+	void Display() { cout << this->num << endl; }
+};
+
+int Distance(Dot& a) {
+	return abs(0 - a.GetX()) + abs(0 - a.GetY());
+}
+
+bool Cmp(Dot& a, Dot& b) {
+	return Distance(a) < Distance(b);
+}
+
+int main() {
+	int n;
+	int x, y;
+	vector<Dot> dotV;
+	cin >> n;
+	dotV.reserve(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		dotV.emplace_back(x, y, i + 1);
+	}
+
+	sort(dotV.begin(), dotV.end(), Cmp);
+
+	for (int i = 0; i < dotV.size(); i++) {
+		dotV[i].Display();
+	}
+
+	return 0;
+}
