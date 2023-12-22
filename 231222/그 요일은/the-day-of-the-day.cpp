4ceed820668ea459eb30586cee3fb9e1ab3599e5
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-22 15:36:22 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231222/그 요일은/the-day-of-the-day.cpp
+++ 231222/그 요일은/the-day-of-the-day.cpp
@@ -20,30 +20,18 @@ int main() {
 	int m2, d2;
 	string day;
 	int sub = 0;
-	int cnt = 0;
+	int cnt = 1;
 
 	cin >> m1 >> d1;
 	cin >> m2 >> d2;
 	cin >> day;
 	sub = Date(m2, d2) - Date(m1, d1);
 	cnt = sub / 7;
-
-	switch (sub % 7) {
-	case 0:
-		if (day == DAY[0]) cnt += 1;
-	case 1:
-		if (day == DAY[1]) cnt += 1;
-	case 2:
-		if (day == DAY[2]) cnt += 1;
-	case 3:
-		if (day == DAY[3]) cnt += 1;
-	case 4:
-		if (day == DAY[4]) cnt += 1;
-	case 5:
-		if (day == DAY[5]) cnt += 1;
-	case 6:
-		if (day == DAY[6]) cnt += 1; break;
+	
+	for (int i = 0; i <= sub % 7; i++) {
+		if (DAY[i] == day) cnt++;
 	}
+
 	
 	cout << cnt << endl;
 }
