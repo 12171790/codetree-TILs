8411d8ca7111e1c22b41614572802285fb345a7f
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-26 17:40:11 +0900

    Title: 연속되는 수 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231226/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, e;
+	int max = 0, cnt = 0;
+	vector<int> v;
+	cin >> n;
+	v.reserve(n);
+	for (int i = 0; i < n; i++) {
+		cin >> e;
+		v.emplace_back(e);
+	}
+
+	for (int i = 1; i < v.size(); i++) {
+		if (v[i] > 0 && v[i - 1] > 0) cnt++;
+		else if (v[i] < 0 && v[i - 1] < 0) cnt++;
+		else cnt = 0;
+
+		if (cnt > max) 	max = cnt;
+	}
+
+	cout << max + 1;
+}
