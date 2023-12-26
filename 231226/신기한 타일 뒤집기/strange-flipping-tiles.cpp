eb5dbd6548fb8829da98199f323486151765af6d
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-26 13:59:48 +0900

    Title: 신기한 타일 뒤집기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231226/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Stack(vector<int>& v, int& move, char& dir, int& prev) {
+
+	
+	//-1 검은색, 1 흰색
+	if (dir == 'R') { //검은색
+		for (int i = prev; i < prev + move; i++) {
+			v[i] = -1;
+		}
+		return prev + move;
+	}
+	else if (dir == 'L') { //흰색
+		for (int i = prev - 1; i >= prev - move; i--) {
+			v[i] = 1;
+		}
+		return prev - move;
+	}
+
+}
+
+int main() {
+	vector<int> v(200000, 0);
+	int n, move = 0, prev = 100000;
+	int white = 0, black = 0, gray = 0;
+	char dir = 0;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> move >> dir;
+		prev = Stack(v, move, dir, prev);
+	}
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i] == 1) white++;
+		else if (v[i] == -1) black++;
+	}
+	cout << white << " " << black  << endl;
+
+	return 0;
+}
