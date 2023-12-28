02da350c9454e4597478d6f92e5604c9ca0acb9f
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2023-12-29 08:53:58 +0900

    Title: 되돌아오기 2, Time: 1008ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 231229/되돌아오기 2/come-back-2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool inRange(int r, int c, int n, int m) {
+	return (r >= 0 && r < n && c >= 0 && c < m);
+}
+
+int main() {
+	char order; // 명령 L, R, F
+	//북 동 남 서
+	int dx[4] = { 0, 1, 0, -1 };
+	int dy[4] = { 1, 0, -1, 0 };
+	int dirNum = 0;
+	int cntSecond = 0; // 초
+	int nx = 0, ny = 0; // 현재 위치
+	bool returnToPoint = false;
+
+
+	while (true) {
+		cin >> order;
+
+		if (order == 'L') {
+			dirNum = (dirNum + 3) % 4;
+		}
+		else if (order == 'R') {
+			dirNum = (dirNum + 1) % 4;
+		}
+		else if (order == 'F') {
+			nx += dx[dirNum];
+			ny += dy[dirNum];
+		}
+		cntSecond++;
+
+		if (nx == 0 && ny == 0) {
+			returnToPoint = true;
+			break;
+		}
+	}
+
+
+	if (returnToPoint) cout << cntSecond;
+	else cout << -1;
+
+
+
+	return 0;
+}
