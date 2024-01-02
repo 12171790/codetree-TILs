6ea679fca1ff08119896ca7e1fd26fbf688d5896
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-01-02 17:16:44 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240102/Carry 피하기 2/escaping-carry-2.cpp
+++ 240102/Carry 피하기 2/escaping-carry-2.cpp
@@ -22,11 +22,17 @@ int main() {
 		v.emplace_back(e);
 	}
 
+	/*
+	* 7206 3080 602 => 6 0 2
+	* 720 308 60 => 0 8 0
+	* 72 30 6 => 2 0 6
+	* 7 3 0 =>
+	*/
 	for (int i = 0; i < v.size() - 2; i++) {
 		for (int j = i + 1; j < v.size() - 1; j++) {
 			for (int k = j + 1; k < v.size(); k++) {
 				firstNum = v[i], secondNum = v[j], thirdNum = v[k];
-				while (firstNum && secondNum && thirdNum) {
+				while (firstNum != 0 || secondNum != 0 || thirdNum != 0) {
 					divSum = (firstNum % 10) + (secondNum % 10) + (thirdNum % 10);
 					firstNum /= 10, secondNum /= 10, thirdNum /= 10;
 
@@ -35,9 +41,10 @@ int main() {
 						break;
 					}
 				}
-
+				
 				sum = v[i] + v[j] + v[k];
 				if (!carry) {
+
 					maxSum = max(maxSum, sum);
 				}
 				carry = false;
