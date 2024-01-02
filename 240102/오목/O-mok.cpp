046a984b84a46d8ce982fe926bc0c9d256a48e85
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-01-02 19:35:49 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240102/오목/O-mok.cpp
+++ 240102/오목/O-mok.cpp
@@ -13,9 +13,9 @@ bool inRange(int r, int c) {
 int main() {
 	int e;
 	int cnt = 0;
-	//오른쪽 대각선 아래
-	int dr[3] = { 0, 1, 1 };
-	int dc[3] = { 1, 1, 0 };
+	//동쪽 기준 시계 방향
+	int dr[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+	int dc[8] = { 1, 1, 0, -1, -1, -1, 0, 1 };
 	int dirNum = 0;
 	int nr, nc;
 	vector<vector<int>> v(19, vector<int>(19, 0));
@@ -30,46 +30,32 @@ int main() {
 	for (int i = 0; i < v.size() - 4; i++) {
 		for (int j = 0; j < v[0].size() - 4; j++) {
 			if (v[i][j] == 1) {
-				for (int k = 0; k < 3; k++) {
+				for (int k = 0; k < 8; k++) {
 					dirNum = k;
 					nr = i, nc = j;
-					while (v[nr][nc] == 1) {
+					while (inRange(nr, nc) && v[nr + dr[dirNum]][nc + dc[dirNum]] == 1) {
 						cnt++;
 						nr += dr[dirNum], nc += dc[dirNum];
 					}
-					if (cnt == 5) {
-						cout << 1 << endl;
-						switch (dirNum) {
-						case 0:
-							cout << nr + 1 << " " << nc - 2 << endl; break;
-						case 1:
-							cout << nr - 2 << " " << nc - 2 << endl; break;
-						case 2:
-							cout << nr - 2 << " " << nc + 1 << endl; break;
-						}
+					if (cnt == 4) {
+						cout << 2 << endl;
+						cout << nr + 1 - 2 * dr[dirNum] << " " << nc + 1 - 2 * dc[dirNum] << endl;
 						return 0;
 					}
 					cnt = 0;
 				}
 			}
 			else if (v[i][j] == 2) {
-				for (int k = 0; k < 3; k++) {
+				for (int k = 0; k < 8; k++) {
 					dirNum = k;
 					nr = i, nc = j;
-					while (v[nr][nc] == 2) {
+					while (inRange(nr, nc) && v[nr + dr[dirNum]][nc + dc[dirNum]] == 2) {
 						cnt++;
 						nr += dr[dirNum], nc += dc[dirNum];
 					}
-					if (cnt == 5) {
+					if (cnt == 4) {
 						cout << 2 << endl;
-						switch (dirNum) {
-						case 0:
-							cout << nr + 1 << " " << nc - 2 << endl; break;
-						case 1:
-							cout << nr - 2 << " " << nc - 2 << endl; break;
-						case 2:
-							cout << nr - 2 << " " << nc + 1 << endl; break;
-						}
+						cout << nr + 1 - 2 * dr[dirNum] << " " << nc + 1 - 2 * dc[dirNum] << endl;
 						return 0;
 					}
 					cnt = 0;
@@ -77,6 +63,8 @@ int main() {
 			}
 		}
 	}
+
+	cout << 0 << endl;
 
 	return 0;
 }
