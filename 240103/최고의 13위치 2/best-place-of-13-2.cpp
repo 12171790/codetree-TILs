9360d764288402f56e3edccc7c2e52f226769d46
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-01-03 13:49:00 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240103/최고의 13위치 2/best-place-of-13-2.cpp
+++ 240103/최고의 13위치 2/best-place-of-13-2.cpp
@@ -29,27 +29,26 @@ int main() {
 	for (int i = 0; i < N; i++) {
 		for (j = 0; j < N - 2; j++) {
 			cntCoin = v[i][j] + v[i][j + 1] + v[i][j + 2];
-		}
-		for (int k = i; k < N; k++) {
-			if (k == i) {
-				for (int l = j + 3; l < N - 2; l++) {
-					
-					if (inRange(k, l + 2)) cntCoin += v[k][l] + v[k][l + 1] + v[k][l + 2];
-				
-					if (cntCoin > maxCoin) maxCoin = cntCoin;
+			for (int k = i; k < N; k++) {
+				if (k == i) {
+					for (int l = j + 3; l < N - 2; l++) {
+						if (inRange(k, l + 2)) cntCoin += v[k][l] + v[k][l + 1] + v[k][l + 2];
 
-					cntCoin = v[i][j] + v[i][j + 1] + v[i][j + 2];
+						if (cntCoin > maxCoin) maxCoin = cntCoin;
 
+						cntCoin = v[i][j] + v[i][j + 1] + v[i][j + 2];
+
+					}
 				}
-			}
-			else {
-				for (int l = 0; l < N - 2; l++) {
-					
-					cntCoin += v[k][l] + v[k][l + 1] + v[k][l + 2];
+				else {
+					for (int l = 0; l < N - 2; l++) {
 
-					if (cntCoin > maxCoin) maxCoin = cntCoin;
+						cntCoin += v[k][l] + v[k][l + 1] + v[k][l + 2];
 
-					cntCoin = v[i][j] + v[i][j + 1] + v[i][j + 2];
+						if (cntCoin > maxCoin) maxCoin = cntCoin;
+
+						cntCoin = v[i][j] + v[i][j + 1] + v[i][j + 2];
+					}
 				}
 			}
 		}
