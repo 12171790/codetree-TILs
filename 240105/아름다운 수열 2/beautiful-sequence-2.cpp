8854d21de43498e6a0880a4d02181ebe57cc3e6e
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-01-05 13:40:49 +0900

    Title: 아름다운 수열 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240105/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 240105/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -12,7 +12,6 @@ int main() {
 	int N, M;
 	int ele;
 	int cntAnswer = 0;
-	bool dup = false;
 	int cnt = 0;
 	vector<int> A;
 	vector<int> B;
@@ -29,28 +28,30 @@ int main() {
 		B.emplace_back(ele);
 	}
 
-	for (int i = 0; i <= A.size() - B.size(); i++) {
-		vector<int> v(B.size(), 0);
-		for (int j = 0; j < B.size(); j++) {
+	if (N >= M) {
+		for (int i = 0; i <= A.size() - B.size(); i++) {
+			vector<int> v(B.size(), 0);
+			for (int j = 0; j < B.size(); j++) {
 
-			for (int k = 0; k < B.size(); k++) {
-				if (A[i + j] == B[k]) {
-					if (v[k] == 1) continue;
-					else {
-						v[k] = 1;
-						cnt++;
-						break;
+				for (int k = 0; k < B.size(); k++) {
+					if (A[i + j] == B[k]) {
+						if (v[k] == 1) continue;
+						else {
+							v[k] = 1;
+							cnt++;
+							break;
+						}
 					}
 				}
+
 			}
 
-		}
+			if (cnt == B.size()) {
+				cntAnswer++;
+			}
 
-		if (cnt == B.size()) {
-			cntAnswer++;
+			cnt = 0;
 		}
-
-		cnt = 0;
 	}
 
 	cout << cntAnswer << endl;
