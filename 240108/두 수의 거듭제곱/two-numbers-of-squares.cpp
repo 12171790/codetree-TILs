6e76d22e8c1b4078b0fc6fb4cc3581f36e82d4ad
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-01-08 22:48:29 +0900

    Title: 두 수의 거듭제곱, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int p(int a, int b) {
+    return pow(a, b);
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << p(a, b);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
