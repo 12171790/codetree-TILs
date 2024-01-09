8722ab193ffbe58b1e4688808f5b9be4ab1e39fc
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-01-09 17:24:20 +0900

    Title: 개발자의 능력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240109/개발자의 능력 2/ability-of-developer-2.cpp
+++ 240109/개발자의 능력 2/ability-of-developer-2.cpp
@@ -7,42 +7,20 @@
 #include <cmath>
 using namespace std;
 
-int arr[6];
-
-int getSub(int i, int j, int k, int l) {
-	int totalSum = 0;
-	int minSum = 0;
-	int maxSum = 0;
-	for (int i = 0; i < 6; i++) totalSum += arr[i];
-
-	int sumFirstGroup = arr[i] + arr[j];
-	int sumSecondGroup = arr[k] + arr[l];
-	int sumThirdGroup = totalSum - (sumFirstGroup + sumSecondGroup);
-
-	minSum = min(sumFirstGroup, min(sumSecondGroup, sumThirdGroup));
-	maxSum = max(sumFirstGroup, max(sumSecondGroup, sumThirdGroup));
-
-	return maxSum - minSum;
-}
-
 int main() {
 	int minSub = INT_MAX;
+	int arr[6];
 	int cnt = 0;
 	for (int i = 0; i < 6; i++) cin >> arr[i];
 
-	for (int i = 0; i <= 6 - 4; i++) {
-		for (int j = i + 1; j <= 6 - 3; j++) {
-			for (int k = j + 1; k <= 6 - 2; k++) {
-				for (int l = k + 1; l < 6; l++) {
-					minSub = min(minSub, getSub(i, j, k, l));
-					minSub = min(minSub, getSub(i, k, j, l));
-				}
-			}
-		}
-	}
-	cout << minSub << endl;
-}
+	sort(arr, arr + 6);
 
-//720
-// 2 2 2 3
-//
+	int sumFirstGroup = arr[0] + arr[5];
+	int sumSecondGroup = arr[1] + arr[4];
+	int sumThirdGroup = arr[2] + arr[3];
+	int minSum = min(sumFirstGroup, min(sumSecondGroup, sumThirdGroup));
+	int maxSum = max(sumFirstGroup, max(sumSecondGroup, sumThirdGroup));
+
+
+	cout << maxSum - minSum << endl;
+}
