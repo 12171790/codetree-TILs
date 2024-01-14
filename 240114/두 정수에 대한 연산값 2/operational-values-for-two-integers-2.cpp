9d3cf50fcb43fe0960bf79680c789961c5358735
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-01-14 11:52:53 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240114/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
+++ 240114/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 void SpecialCalculator(int& a, int & b) {
     a > b ? a *= 2, b += 10 : a += 10, b *= 2;
-
-    cout << a << " " << b;
 }
 
 int main() {
     int a, b;
     cin >> a >> b;
     SpecialCalculator(a, b);
+
+    cout << a << " " << b;
     // 여기에 코드를 작성해주세요.
     return 0;
 }
