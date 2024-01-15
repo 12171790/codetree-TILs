daf9517f860cbcab9437b4f8b36bbea6f01a88f4
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-01-15 17:02:57 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240115/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int N, K; // 폭탄 N개, 특정 거리 K
+	int e;
+	int max_num = INT_MIN;
+	vector<int> v;
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++) {
+		cin >> e;
+		v.emplace_back(e);
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j <= K + i; j++) {
+			if (j >= N) break; // index 범위 벗어나지 않도록
+
+			if (v[i] == v[j]) max_num = max(max_num, v[i]);
+		}
+	}
+
+	cout << max_num;
+	return 0;
+}
