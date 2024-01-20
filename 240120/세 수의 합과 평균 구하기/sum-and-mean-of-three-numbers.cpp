be86808019cc697c3c8a4d9d79140acb34e7f565
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-01-20 23:08:55 +0900

    Title: 세 수의 합과 평균 구하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int sum = a + b + c;
+    int avg = sum / 3.0;
+    cout << sum << endl << avg;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
