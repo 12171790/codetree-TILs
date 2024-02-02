94a0e6097accd6292428b1750c495dfc9ed3c2fb
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-02-02 17:30:06 +0900

    Title: 초기 수열 복원하기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240202/초기 수열 복원하기/restore-initial-sequence.cpp
+++ 240202/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main() {
 	int N, e;
 	vector<int> v;
-    cin >> N;
+	cin >> N;
 	for (int i = 0; i < N - 1; i++) {
 		cin >> e;
 		v.emplace_back(e);
@@ -19,13 +19,18 @@ int main() {
 
 	for (int i = 1; i <= N; i++) { //첫번째 숫자 지정
 		vector<int> temp;
-        bool indup = false;
+		bool indup = false;
 		temp.emplace_back(i);
 		for (int j = 0; j < N - 1; j++) {
-			temp.emplace_back(v[j] - temp[j]);
+				temp.emplace_back(v[j] - temp[j]);
 		}
 
 		for (int j = 1; j < N; j++) {
+			if (temp[j] == 0 || temp[j] > N) {
+				indup = true;
+				break;
+			}
+
 			if (temp[j] == temp[j - 1]) {
 				indup = true;
 				break;
