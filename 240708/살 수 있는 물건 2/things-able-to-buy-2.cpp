cef2904e98b784b36214d07fd9b95b3f52a31dd7
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-07-08 16:36:27 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240708/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int money = 0;
+
+    cin >> money;
+
+    if (money >= 3000) {
+        cout << "book" << endl;
+    } else if (money >= 1000) {
+        cout << "mask" << endl;
+    } else if (money >= 500) {
+        cout << "pen" << endl;
+    } else {
+        cout << "no" << endl;
+    }
+    return 0;
+}
