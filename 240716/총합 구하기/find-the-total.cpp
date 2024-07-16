59e633a1e86f198aca27a38468cb99395260ea88
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-07-16 15:02:43 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/총합 구하기/find-the-total.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int sum = 0;
+
+    cin >> a >> b;
+
+    for (int i = a; i <= b; i++) {
+        if (i % 6 == 0) {
+            if (i % 8 != 0) {
+                sum += i;
+            }
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+}
