a36c7d685897ac18f9853d76924690f9f3f99020
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2024-07-25 22:54:05 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240725/미는 횟수/number-of-pushes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a;
+    string b;
+    int cnt = 0;
+    bool eql = false;
+
+    cin >> a >> b;
+
+    for (int i = 1; i < a.length(); i++) {
+        a = a.substr(1, a.length() - 1) + a.substr(0, 1);
+
+        if (a.compare(b) == 0) {
+            cnt = i;
+            eql = true;
+        }
+    }
+
+    if (eql) {
+        cout << cnt;
+    } else {
+        cout << -1;
+    }
+
+    return 0;
+}
