5900d14a2cf73e178a2cb29560b0f47093bd4bff
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2025-02-04 08:17:15 +0900

    Title: 점수대 파악하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250204/점수대 파악하기/find-out-the-score-range.py
+++ 250204/점수대 파악하기/find-out-the-score-range.py
@@ -2,6 +2,9 @@ arr = list(map(int, input().split()))
 scoreArr = [0 for _ in range(11)]
 
 for el in arr:
+    if el == 0:
+        break
+
     scoreArr[el // 10] += 1
 
 for i in range(len(scoreArr) - 1, 0, -1):
