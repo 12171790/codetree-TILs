ee129b06c32dbbeabb323fe119702b25dfc338ea
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2025-02-04 08:01:49 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250204/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+
+for el in arr:
+    if el == 0:
+        break
+    
+    if el % 2 == 1:
+        print(el + 3, end=' ')
+    else:
+        print(el // 2, end= ' ')
+        
