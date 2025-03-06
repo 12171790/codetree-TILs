c660152203e4969b1701cdb3b9503a73e64d8377
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2025-03-06 10:26:55 +0900

    Title: 함수를 이용한 소수 판별, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250306/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 250306/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -2,7 +2,11 @@ def is_prime(n):
     for i in range(2, n):
         if n % i == 0:
             return False
-    return True
+    
+    if i != 1:
+        return True
+    else:
+        return False
 
 def prime_sum(a, b):
     sum = 0;
