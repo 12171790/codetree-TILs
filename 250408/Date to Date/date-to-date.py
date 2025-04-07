cdc2f40ae2100dbc23f1212e733fd804646cf79a
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2025-04-08 08:06:57 +0900

    Title: Date to Date, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250408/Date to Date/date-to-date.py
+++ 250408/Date to Date/date-to-date.py
@@ -2,16 +2,12 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 m1, d1, m2, d2 = map(int, input().split())
 
-firstNum, secondNum = 0, 0
+firstNum, secondNum = d1 - 1, d2
 
-for i in range(1, m1 + 1):
+for i in range(1, m1):
     firstNum += num_of_days[i]
 
-firstNum += d1
-
-for i in range(1, m2 + 1):
+for i in range(1, m2):
     secondNum += num_of_days[i]
-
-secondNum += d2
 
 print(secondNum - firstNum)
