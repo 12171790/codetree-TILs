36bf176ca502e1e3b6a524741cbe4ffb2f5f4c46
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2025-04-29 08:56:26 +0900

    Title: 거울에 레이저 쏘기 2, Time: 5084ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

---
+++ 250429/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,38 @@
+n = int(input())
+arr = [input() for _ in range(n)]
+
+k = int(input())
+
+def in_range(x, y):
+    return x >= 0 and x < n and y >= 0 and y < n
+
+# 빛 진행 방향  동 북 남 서
+Ldxs, Ldys = [-1, 0, 0, 1], [0, 1, -1, 0]
+# 빛 진행 방향  남 서 북 동
+Rdxs, Rdys = [0, -1, 0, 1], [1, 0, -1, 0]
+pos = k // n
+
+if pos == 0:
+    x, y = 0, k - 1
+elif pos == 1:
+    x, y = k - n - 1, n - 1
+elif pos == 2:
+    x, y = n - 1, 3 * n - k
+elif pos == 3:
+    x, y = 4 * n - k, 0
+
+count = 0
+dir_num = 0
+if arr[x][y] == '/':
+    dir_num = (pos + 1) % 4
+elif arr[x][y] == '\\':
+    dir_num = pos
+
+while(in_range(x, y)):
+    if arr[x][y] == '/':
+        x, y = x + Ldxs[dir_num], y + Ldys[dir_num]
+    elif arr[x][y] == '\\':
+        x, y = x + Rdxs[dir_num], y + Rdys[dir_num]
+    dir_num = 3 - dir_num
+    count += 1
+print(count)
