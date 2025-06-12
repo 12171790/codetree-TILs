a1ab7f8ecff2386295f222495acd3ee1bc783738
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2025-06-12 22:38:04 +0900

    Title: 2줄 출력, Time: 80ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250612/2줄 출력/print-two-lines.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello\n");
+        sb.append("World\n");
+
+        System.out.println(sb);
+    }
+}
