918cf66c33a5318433fb9c435ff214d8e48055d6
Bae Ki Woong <112368201+12171790@users.noreply.github.com>
2025-06-18 18:33:02 +0900

    Title: 다양하게 출력, Time: 99ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250618/다양하게 출력/print-in-variety.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Total days in Year");
+        System.out.println("365");
+        System.out.println("Circumference rate");
+        String piStr = String.valueOf(Math.PI);
+        piStr = piStr.substring(0, 12);
+        System.out.println(piStr);
+    }
+}
